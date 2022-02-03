6341bea80c4a4416c8bc22e8509e7478e37074e9
Garrett <58259411+chemonoworld@users.noreply.github.com>
2022-02-04 06:37:52 +0900

    Update RNG.sol

--- RNG.sol
+++ RNG.sol
@@ -1,1 +1,36 @@
+pragma solidity ^0.5.0;
 
+
+contract RandomNumberGenerator {
+    uint[1000] tokenIds; // tokenId : 1~1000
+    uint preSaleQuantity = 1000;
+    uint remainingTokenCount = preSaleQuantity;
+
+    function _generateRandomNumber (uint requiredSeed) public view returns (uint) {
+        require(remainingTokenCount > 0);
+        return uint(keccak256(abi.encodePacked(requiredSeed, blockhash(block.number), msg.sender))) % remainingTokenCount;
+    }
+
+    function _initiateNameTagId () private {
+        for (uint i=0 ; i <preSaleQuantity ; i++)
+        {
+            tokenIds[i] = i + 1;
+        }
+    }
+    
+    function _draw (uint requiredSeed) public returns (uint) {
+        require(remainingTokenCount > 0);
+        uint randNum =  _generateRandomNumber(requiredSeed);
+        uint drawNum = tokenIds[randNum]; 
+        tokenIds[randNum] = tokenIds[remainingTokenCount - 1];
+        // + mint
+        remainingTokenCount--;
+        return (drawNum);
+    }
+    
+
+    constructor () public {
+        //initiate pre-sale nft tokenId
+        _initiateNameTagId();
+    }
+}
